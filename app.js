12c594d65fedda39836367fef2d54a02c2281c30
NeoGlanding <neoglanding@protonmail.com>
2021-08-31 21:53:08 +0700

    mongoose connection success

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const mentorRouter = require('./router/mentorRouter');
+const mongoose = require('mongoose');
 
 app.use(express.json());
 
 app.use('/api/mentors', mentorRouter);
 
-// Listen
+// Server
+const db = 'mongodb://localhost:27017/mentors-real'
+mongoose.connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(con => console.log('connected')).catch(err => console.log(err))
+
 app.listen(3000, () => {
     console.log('API Running in port 3000')
 });
